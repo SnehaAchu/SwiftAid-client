e03b37ac4e417ca1359bd63d1d00e5ce39284da8
SnehaAchu <108051782+SnehaAchu@users.noreply.github.com>
2022-07-31 12:39:36 +0530

    Update accept.dart

--- lib/accept.dart
+++ lib/accept.dart
@@ -1,89 +1,105 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
-//import 'package:geolocator/geolocator.dart';
-//import 'package:geocoding/geocoding.dart';
-//import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:users/accept.dart';
+import 'package:users/alert.dart';
+import 'package:users/response.dart';
+import 'package:users/styles.dart';
+import 'package:users/menu.dart';
 
-/*class accept extends StatefulWidget {
-  @override
-  State<accept> createState() => _acceptState();
-}
+void main() => runApp(Main());
 
-class _acceptState extends State<accept> {
-  late GoogleMapController newmap;
-
-  late Position currentposition;
-  var geolocator = Geolocator();
-  String adress = ''; ///////////////////////////////////////////////////////
-
-  void locateposition() async {
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
-    currentposition = position;
-
-    LatLng latLngposition = LatLng(position.latitude, position.longitude);
-    // pos = latLngposition.toString();
-    print(latLngposition);
-    //print(pos);
-    List<Placemark> placemark = await placemarkFromCoordinates(
-        ////////////////////////////
-        latLngposition.latitude,
-        latLngposition
-            .longitude); //////////////////////////////// ( latLngposition doctor de loction aan  )
-    print(placemark); ////////////////////////////
-    Placemark place = placemark[0]; //////////////////////////////
-    adress = //////////////////////////////
-        '${place.street}, ${place.subLocality}, ${place.locality}, ${place.postalCode}, ${place.country}'; ////////////////////////////////
-    setState(() {});
-    CameraPosition cameraPosition =
-        new CameraPosition(target: latLngposition, zoom: 20);
-
-    newmap.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
-  }*/
-class accept extends StatelessWidget {
+class Main extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //return MaterialApp(debugShowCheckedModeBanner: false);
+    return ScreenUtilInit(builder: (context, child) {
+      return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: User(),
+      );
+    });
+  }
+}
 
+class User extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        backgroundColor: Color.fromARGB(255, 255, 255, 255),
-        //drawer: Menubar(),
-        appBar: AppBar(
-          backgroundColor: Color.fromARGB(248, 32, 42, 68),
-          centerTitle: true,
-          title: Text('SwiftAid',
-              style: GoogleFonts.aladin(
-                  fontSize: 30,
-                  color: Color.fromARGB(255, 249, 247, 247),
-                  fontWeight: FontWeight.w100)),
-        ),
-        body: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 100, left: 10, right: 10),
-              child: Container(
-                height: 300,
-                alignment: Alignment.center,
-                child: Text(
-                  "Your request has been accepted! \n\n Doctor arriving from Kanjirapally.",
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.aBeeZee(
-                      fontSize: 20, color: Color.fromARGB(248, 26, 52, 117)),
-                ),
+          backgroundColor: Color.fromARGB(255, 255, 255, 255),
+          drawer: Menubar(),
+          appBar: AppBar(backgroundColor: Color.fromARGB(248, 32, 42, 68)),
+          body: (Column(
+            children: [
+              Container(
+                  height: 100,
+                  alignment: Alignment.topCenter,
+                  //width: ,
+                  child: Padding(
+                    padding: const EdgeInsets.only(top: 35),
+                    child: Text(
+                      "SwiftAid",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.aladin(
+                          fontSize: 50,
+                          fontWeight: FontWeight.w100,
+                          color: Color.fromARGB(248, 32, 42, 68)),
+                    ),
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Container(
+                    //alignment: Alignment.topCenter,
+                    height: 250,
+                    child: Column(
+                      children: [
+                        Text("NOTE: ",
+                            //textAlign: TextAlign.center,
+                            style: GoogleFonts.aBeeZee(
+                                fontSize: 20,
+                                color: Color.fromARGB(255, 170, 170, 172))),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 30, right: 10, top: 30),
+                          child: Text(
+                              "** This app is meant for emergency situations only \n\n ** Do not completely depend on this app for help ",
+                              textAlign: TextAlign.left,
+                              style: GoogleFonts.aBeeZee(
+                                  fontSize: 20,
+                                  color: Color.fromARGB(255, 170, 170, 172))),
+                        )
+                      ],
+                    )),
               ),
-            ),
-            Image.asset(
-              "images/arrive.jpg",
-              color: const Color.fromRGBO(255, 255, 255, 0.645),
-              colorBlendMode: BlendMode.modulate,
-              height: 350,
-              width: 400,
-              alignment: Alignment.bottomCenter,
-            ),
-          ],
-        ),
-      ),
+              SizedBox(
+                  height: 95,
+                  width: 300,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => alert1()));
+                        },
+                        child: Text(
+                          "Request  for  Help",
+                          //textAlign: TextAlign.center,
+                          style: buttonTextStyle,
+                        ),
+                        style: logbutton),
+                  )),
+              Image.asset(
+                "images/phone.jpg",
+                color: const Color.fromRGBO(255, 255, 255, 0.645),
+                colorBlendMode: BlendMode.modulate,
+                height: 255,
+                width: 400,
+                alignment: Alignment.bottomCenter,
+              ),
+            ],
+          ))),
     );
   }
 }
